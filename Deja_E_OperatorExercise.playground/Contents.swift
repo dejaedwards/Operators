517b4bd80f796b6ea4d763e8eb888c516d38e275
dejaedwards <67969078+dejaedwards@users.noreply.github.com>
2020-08-12 07:56:14 -0700

    Add files via upload

---
+++ Deja_E_OperatorExercise.playground/Contents.swift
@@ -0,0 +1,13 @@
+import Cocoa
+
+var str = "Hello, playground"
+
+
+//input values
+var numberOne = 6
+var numberTwo = 3
+//Operations and answers
+print("Your answer is: \(numberOne * numberTwo)")
+print("Your answer is: \(numberOne + numberTwo)")
+print("Your answer is: \(numberOne - numberTwo)")
+print("Your answer is: \(numberOne / numberTwo)")
